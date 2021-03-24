93d0dc4e573388d5c5de762457a86ae83ffaa5c3
anaelle97 <anaellecloarec97@gmail.com>
2021-03-24 10:51:24 +0100

    commit 5

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -33,21 +33,5 @@ class WishController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/wishes/list/create", name="wish_create")
-     */
-    public function createWish(EntityManagerInterface $entityManager)
-    {
-        $bucket = new BucketList();
-        $bucket->setTitle('Deathstroke');
-        $bucket->setDescription("Restore the Snyderverse");
-        $bucket->setAuthor('Slade Wilson');
-        $bucket->setIsPublished(1);
-        $bucket->setDateCreated(new \DateTime());
 
-        $entityManager->persist($bucket);
-        $entityManager->flush();
-
-        die('ok');
-    }
 }
